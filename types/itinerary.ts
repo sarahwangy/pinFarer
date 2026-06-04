91ef704eb2d01baa9f4121c902bd2b3eb7731a93
Sarah Wang <sarahwangdk@gmail.com>
2026-06-04 13:03:06 +1000

    feat: add itinerary types

---
+++ types/itinerary.ts
@@ -0,0 +1,21 @@
+export interface Activity {
+  time: string
+  name: string
+  desc: string
+  color: 'coral' | 'mint' | 'amber' | 'lavender'
+}
+
+export interface DayPlan {
+  day: string
+  title: string
+  tag: string
+  activities: Activity[]
+}
+
+export interface Itinerary {
+  title: string
+  days: DayPlan[]
+  pinCount: number
+  style: string
+  totalDays: number
+}
